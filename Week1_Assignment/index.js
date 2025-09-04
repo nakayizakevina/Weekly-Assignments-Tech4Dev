3d5f36262efd445f7611ab8ab38b2fed40f92326
Kevina <kevinanakayiza@gmail.com>
2025-09-04 17:33:25 +0300

    This is my first weekly Assignment with Tech4Deve and it is all about the basics have learnt on JS

---
+++ Week1_Assignment/index.js
@@ -0,0 +1,16 @@
+let num1;
+let num2;
+let name;
+
+num1 = 2;
+num2 = 5;
+name = prompt ("what is your name");
+
+let add = num1 + num2;
+let minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+console.log(name);
+console.log(add);
+console.log("Hello " + name + ", the result of " + num1 + " + " + num2 + " is " + add);
